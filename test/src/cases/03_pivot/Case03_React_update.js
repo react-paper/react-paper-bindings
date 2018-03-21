3d9ffa24985abe46856adb027cae12140b1a541f
kota65535 <kota65535@gmail.com>
2018-03-21 13:35:53 +0900

    add tests based on visual regression test

---
+++ test/src/cases/03_pivot/Case03_React_update.js
@@ -0,0 +1,84 @@
+import React from "react";
+import {Circle, Ellipse, Group, Rectangle, View} from "../../../../src/index";
+import {Point} from "paper";
+import {createGridLines} from "../common";
+import qs from "query-string";
+
+export default class Case03_React extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.parseQueryString(props)
+    console.log(`applyMatrix=${this.applyMatrix}`)
+    this.state = {
+      pivot: new Point(0, 100),
+      position: new Point(200,200),
+    }
+  }
+
+  parseQueryString(props) {
+    const params =qs.parse(props.location.search)
+    this.applyMatrix = (params.applyMatrix == 'false') ? false : true
+  }
+
+  componentDidMount() {
+    this.setState({
+      pivot: new Point(100, 0),
+      position: new Point(200,300),
+    })
+  }
+
+  render() {
+    const x = 100, y = 100
+
+    return (
+      <View width={800}
+            height={600}
+            settings={{
+              applyMatrix: this.applyMatrix
+            }}
+      >
+        {createGridLines(800, 600, 100)}
+
+        <Rectangle
+          pivot={new Point(0, 100)}
+          position={new Point(500, 200)}
+          size={[150, 100]}
+          fillColor={'black'}
+        />
+
+        <Group name={'reactLogo'}
+               pivot={this.state.pivot}
+               position={this.state.position}
+        >
+          <Ellipse
+            position={new Point(x, y)}
+            size={[70, 25]}
+            strokeWidth={2.5}
+            strokeColor={'#61DAFB'}
+          />
+          <Ellipse
+            position={new Point(x, y)}
+            rotation={120}
+            size={[70, 25]}
+            strokeWidth={2.5}
+            strokeColor={'#61DAFB'}
+          />
+          <Ellipse
+            position={new Point(x, y)}
+            rotation={240}
+            size={[70, 25]}
+            strokeWidth={2.5}
+            strokeColor={'#61DAFB'}
+          />
+          <Circle
+            position={new Point(x, y)}
+            fillColor={'#61DAFB'}
+            radius={7}
+          />
+        </Group>
+      </View>
+    )
+  }
+}
+
---
+++ test/src/cases/04_rotation/Case04_React.js
@@ -0,0 +1,75 @@
+import React from "react";
+import {Circle, Ellipse, Group, Rectangle, View} from "../../../../src/index";
+import {Point} from "paper";
+import {createGridLines} from "../common";
+import qs from "query-string";
+
+export default class Case04_React extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.parseQueryString(props)
+    console.log(`applyMatrix=${this.applyMatrix}`)
+  }
+
+  parseQueryString(props) {
+    const params =qs.parse(props.location.search)
+    this.applyMatrix = (params.applyMatrix == 'false') ? false : true
+  }
+
+  render() {
+    const x = 100, y = 100
+
+    return (
+      <View width={800}
+            height={600}
+            settings={{
+              applyMatrix: this.applyMatrix
+            }}
+      >
+        {createGridLines(800, 600, 100)}
+
+        <Rectangle
+          pivot={new Point(0, 100)}
+          position={new Point(500, 200)}
+          rotation={30}
+          size={[150, 100]}
+          fillColor={'black'}
+        />
+
+        <Group name={'reactLogo'}
+               pivot={new Point(0, 100)}
+               position={new Point(200, 200)}
+               rotation={30}
+        >
+          <Ellipse
+            position={new Point(x, y)}
+            size={[70, 25]}
+            strokeWidth={2.5}
+            strokeColor={'#61DAFB'}
+          />
+          <Ellipse
+            position={new Point(x, y)}
+            rotation={120}
+            size={[70, 25]}
+            strokeWidth={2.5}
+            strokeColor={'#61DAFB'}
+          />
+          <Ellipse
+            position={new Point(x, y)}
+            rotation={240}
+            size={[70, 25]}
+            strokeWidth={2.5}
+            strokeColor={'#61DAFB'}
+          />
+          <Circle
+            position={new Point(x, y)}
+            fillColor={'#61DAFB'}
+            radius={7}
+          />
+        </Group>
+      </View>
+    )
+  }
+}
+
